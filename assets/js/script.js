d4551014856c45d714ef2c15d9c57ffa00723b1c
jgncs <joaoguilherme_futebol@hotmail.com>
2023-05-24 13:04:25 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.btn-inicio, .img-2, .footer', { origin: 'bottom' });
+ScrollReveal().reveal('.left, .text, .contato', { origin: 'top' });
+ScrollReveal().reveal('.img-grid, .img-1', { origin: 'left' });
+ScrollReveal().reveal('.heading, .img-3, .right', { origin: 'right' });
